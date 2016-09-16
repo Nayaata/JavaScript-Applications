a9d55f1e6b496f418b0e7cea39ac18e38fcb42a2
Nayaata <n.m.keranova@gmail.com>
2016-09-16 11:41:09 +0300

    Demo version of PromisesAndAsynchronousProgramming homework

---
+++ PromisesAndAsynchronousProgramming/Task 1. GeoLocation/script.js
@@ -0,0 +1,5 @@
+/**
+ Task 1. GeoLocation
+ Write script that gets the users location and renders a image with the map of that location.
+ Use ES2015 Promise
+ */
---
+++ PromisesAndAsynchronousProgramming/Task 2. Popup then load/script.js
@@ -0,0 +1,5 @@
+/**
+ Task 2. Popup then load
+ Write script that shows a message in a popup <div> and after 2 seconds redirects to a different site.
+ Use setTimeout within a Promise
+ */
